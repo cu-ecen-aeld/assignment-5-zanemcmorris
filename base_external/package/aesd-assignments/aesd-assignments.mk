2e2a6819aa7a8d5d28779910e1507ab6b02da07c
zanemcmorris <zamc8851@colorado.edu>
2026-03-14 10:44:34 -0600

    Working nine?

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 75885ef3d7e850ba01dae14bff0f9ca3b1d5347f
+AESD_ASSIGNMENTS_VERSION = 12db51dbf1f0fb521eee68de0eb5fbb9d1326449
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- full-test.sh
+++ full-test.sh
@@ -43,4 +43,5 @@ else
     echo "Missing conf/assignment.txt, no assignment to run"
     exit 1
 fi
+
 exit ${unit_test_rc}
